c0a342971c6535efbf4a5051fae7ced2ecd07f6d
Snigdha Jyoti Boruah <askme.snigdha@gmail.com>
2025-10-23 11:48:22 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10047620
+    "zoneId": 10081637
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
