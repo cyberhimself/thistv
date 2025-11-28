43b56d1a9331e7a5128e638329b99802b831be76
Snigdha Jyoti Boruah <81152493+cyberhimself@users.noreply.github.com>
2025-11-29 00:50:52 +0530

    Killing Me Softly (2002)

--- imdb_scraper.py
+++ imdb_scraper.py
@@ -40,7 +40,7 @@ def get_imdb_poster_url(imdb_id: str) -> str:
 
 
 if __name__ == "__main__":
-    imdb_id = "tt14369780"
+    imdb_id = "tt0250468"
     poster_url = get_imdb_poster_url(imdb_id)
     if poster_url:
         print("✅ Poster URL:", poster_url)
