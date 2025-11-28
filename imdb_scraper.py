c45b2679d4cf80e6c8a78434efddd6ae083a5753
Snigdha Jyoti Boruah <81152493+cyberhimself@users.noreply.github.com>
2025-11-28 14:40:00 +0530

    Touch Chesi Chudu (2018)

--- imdb_scraper.py
+++ imdb_scraper.py
@@ -40,7 +40,7 @@ def get_imdb_poster_url(imdb_id: str) -> str:
 
 
 if __name__ == "__main__":
-    imdb_id = "tt28282341"
+    imdb_id = "tt6468814"
     poster_url = get_imdb_poster_url(imdb_id)
     if poster_url:
         print("✅ Poster URL:", poster_url)
