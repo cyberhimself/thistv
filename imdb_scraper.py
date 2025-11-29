5512fd08805b67d0af175fa7df46ab865727ac08
Snigdha Jyoti Boruah <81152493+cyberhimself@users.noreply.github.com>
2025-11-29 13:32:08 +0530

    21 Bridges (2019)

--- imdb_scraper.py
+++ imdb_scraper.py
@@ -40,7 +40,7 @@ def get_imdb_poster_url(imdb_id: str) -> str:
 
 
 if __name__ == "__main__":
-    imdb_id = "tt0250468"
+    imdb_id = "tt8688634"
     poster_url = get_imdb_poster_url(imdb_id)
     if poster_url:
         print("✅ Poster URL:", poster_url)
