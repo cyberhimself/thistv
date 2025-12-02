335dbd4e3f7e4bd6ce93f980d8c91fb241958bf4
Snigdha Jyoti Boruah <81152493+cyberhimself@users.noreply.github.com>
2025-12-02 11:40:31 +0530

    Kill (2024) Hindi

--- imdb_scraper.py
+++ imdb_scraper.py
@@ -40,7 +40,7 @@ def get_imdb_poster_url(imdb_id: str) -> str:
 
 
 if __name__ == "__main__":
-    imdb_id = "tt5726616"
+    imdb_id = "tt28259207"
     poster_url = get_imdb_poster_url(imdb_id)
     if poster_url:
         print("✅ Poster URL:", poster_url)
