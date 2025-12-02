7b87e37b47d554c917c623e42d7f8d7b71bf1c2b
Snigdha Jyoti Boruah <81152493+cyberhimself@users.noreply.github.com>
2025-12-02 10:35:27 +0530

    Call Me by Your Name (2017)

--- imdb_scraper.py
+++ imdb_scraper.py
@@ -40,7 +40,7 @@ def get_imdb_poster_url(imdb_id: str) -> str:
 
 
 if __name__ == "__main__":
-    imdb_id = "tt7737224"
+    imdb_id = "tt5726616"
     poster_url = get_imdb_poster_url(imdb_id)
     if poster_url:
         print("✅ Poster URL:", poster_url)
