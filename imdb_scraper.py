98dacfc868ba450e39652f15c5ec80f93dcffbf6
Snigdha Jyoti Boruah <81152493+cyberhimself@users.noreply.github.com>
2025-12-11 13:07:12 +0530

    Maze Runner: The Death Cure (2018)

--- imdb_scraper.py
+++ imdb_scraper.py
@@ -40,7 +40,7 @@ def get_imdb_poster_url(imdb_id: str) -> str:
 
 
 if __name__ == "__main__":
-    imdb_id = "tt4046784"
+    imdb_id = "tt4500922"
     poster_url = get_imdb_poster_url(imdb_id)
     if poster_url:
         print("✅ Poster URL:", poster_url)
