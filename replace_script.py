441a72c67ef37c70cc825f74ecda85dfeeba0e46
Snigdha Jyoti Boruah <81152493+cyberhimself@users.noreply.github.com>
2025-12-19 15:47:17 +0530

    Raat Akeli Hai: The Bansal Murders (2025) + Adsterra Update

--- replace_script.py
+++ replace_script.py
@@ -1,8 +1,8 @@
 import os
 
 # Define the old and new domains
-old_domain = "new2.filepress.cloud"
-new_domain = "new3.filepress.cloud"
+old_domain = "intimidatingsideway.com"
+new_domain = "thinnerwithdrewforbear.com"
 
 # Traverse through all directories and files
 for root, dirs, files in os.walk("."):
